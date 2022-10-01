e31b79643eb6e37420a2d62fec975f83fd55ac41
rasulvh <RasulVH@code.local>
2022-10-01 15:06:34 +0400

    added task

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 12;
+            int count = 0;
+
+            for (int i = 1; i < num; i++)
+            {
+                if (num % i == 0)
+                {
+                    count++;
+                }
+                
+            }
+            if (count>2)
+            {
+                Console.WriteLine("Murekkeb ededdir");
+            }
+            else
+            {
+                Console.WriteLine("Sade ededdir");
+            }
+        }
+    }
+}
